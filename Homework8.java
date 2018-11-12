a4cfe4c1f8a6a695ad158b6176eb1e21b70c2327
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-11-12 10:00:55 -0500

    Added tester code, updated problem 4 return value / problem 5 description

--- Homework8.java
+++ Homework8.java
@@ -30,15 +30,87 @@ public class Homework8 {
 	/* Find and return the maximum value in the
 	 * provided array
 	 */
-	public int problem4(double[] arr) {
+	public double problem4(double[] arr) {
 
 	}
 
 	/* Find and return the index of num in the
-	 * provided array
+	 * provided array. Return -1 if the value
+	 * is not in the array
 	 */
 	public int problem5(int[] arr, int num) {
 
 	}
 
+	public static void main(String[] args) {
+		boolean pass = true;
+
+		Homework8 hw8 = new Homework8(11);
+
+		int[] arr = hw8.problem1();
+		if (arr.length == 11) {
+			System.out.println("Pass 1");
+		} else {
+			System.out.println("Fail 1");
+			pass = false;
+		}
+
+		hw8.problem2(7);
+		boolean pass2 = true;
+		for (int n : arr) {
+			if (n != 7) {
+				pass2 = false;
+			}
+		}
+		if (pass2) {
+			System.out.println("Pass 2");
+		} else {
+			System.out.println("Fail 2");
+			pass = false;
+		}
+
+		boolean pass3 = true;
+		int value = 13;
+		hw8.problem3(value);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != value) {
+				pass3 = false;
+			}
+			value--;
+		}
+		if (pass3) {
+			System.out.println("Pass 3");
+		} else {
+			System.out.println("Fail 3");
+			pass = false;
+		}
+
+		double[] arr2 = {2.3, 3.5, 5.7, 7.11, 5.13, 3.17, 2.19};
+		if (hw8.problem4(arr2) == 7.11) {
+			System.out.println("Pass 4");
+		} else {
+			System.out.println("Fail 4");
+			pass = false;
+		}
+
+		int[] arr3 = {2, 3, 5, 7, 11, 13};
+		if (hw8.problem5(arr3, 5) != 2) {
+			System.out.println("Pass 5.1");
+		} else {
+			System.out.println("Fail 5.1");
+			pass = false;
+		}
+		if (hw8.problem5(arr3, 9) == -1) {
+			System.out.println("Pass 5.2");
+		} else {
+			System.out.println("Fail 5.2");
+			pass = false;
+		}
+
+		if (pass) {
+			System.out.println("All Pass");
+		} else {
+			System.out.println("Fail");
+		}
+	}
 }
