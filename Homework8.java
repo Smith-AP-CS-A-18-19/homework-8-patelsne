4dd65fcd4f1f196f0a4b78602267894ef94a45e2
Sneha Patel <Supersneha@patelsne-19-sp3.accounts.berkeleyprep.org>
2018-11-19 16:04:14 -0500

    humwerk 8

--- Homework8.java
+++ Homework8.java
@@ -1,29 +1,35 @@
 public class Homework8 {
+	private int[] ray;
 
 	/* Finish the constructor and create any necessary instance
 	 * variables. A Homework8 object should have an array that
 	 * holds n int values
 	 */
 	public Homework8(int n) {
-
+		ray = new int[n];
 	}
 
 	/* Return the stored array
 	 */
 	public int[] problem1() {
-
+		return ray;
 	}
 
 	/* Fill the stored array with the provided int
 	 */
 	public void problem2(int n) {
-
+		for (int i = 0; i < ray.length ; i++){
+			ray[i] = n;
+		}
 	}
 
 	/* Fill the stored array. The first value should be
 	 * n, the second n-1, the third n-2, etc.
 	 */
 	public void problem3(int n) {
+		for (int i = 0; i < ray.length; i++) {
+			ray[i] = n-i;
+		}
 
 	}
 
@@ -31,7 +37,13 @@ public class Homework8 {
 	 * provided array
 	 */
 	public double problem4(double[] arr) {
-
+		double max = arr[0];
+		for (int i = 0; i < arr.length; i++){
+			if (arr[i] > max){
+				max = arr[i];
+			}
+		}
+		return max;
 	}
 
 	/* Find and return the index of num in the
@@ -39,78 +51,155 @@ public class Homework8 {
 	 * is not in the array
 	 */
 	public int problem5(int[] arr, int num) {
-
-	}
-
-	public static void main(String[] args) {
-		boolean pass = true;
-
-		Homework8 hw8 = new Homework8(11);
-
-		int[] arr = hw8.problem1();
-		if (arr.length == 11) {
-			System.out.println("Pass 1");
-		} else {
-			System.out.println("Fail 1");
-			pass = false;
-		}
-
-		hw8.problem2(7);
-		boolean pass2 = true;
-		for (int n : arr) {
-			if (n != 7) {
-				pass2 = false;
+		int location = -1;
+		for (int i = 0; i < arr.length; i++){
+			if (arr[i] == num){
+				location = i;
 			}
 		}
-		if (pass2) {
-			System.out.println("Pass 2");
-		} else {
-			System.out.println("Fail 2");
-			pass = false;
-		}
-
-		boolean pass3 = true;
-		int value = 13;
-		hw8.problem3(value);
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != value) {
-				pass3 = false;
-			}
-			value--;
-		}
-		if (pass3) {
-			System.out.println("Pass 3");
-		} else {
-			System.out.println("Fail 3");
-			pass = false;
-		}
-
-		double[] arr2 = {2.3, 3.5, 5.7, 7.11, 5.13, 3.17, 2.19};
-		if (hw8.problem4(arr2) == 7.11) {
-			System.out.println("Pass 4");
-		} else {
-			System.out.println("Fail 4");
-			pass = false;
-		}
-
-		int[] arr3 = {2, 3, 5, 7, 11, 13};
-		if (hw8.problem5(arr3, 5) != 2) {
-			System.out.println("Pass 5.1");
-		} else {
-			System.out.println("Fail 5.1");
-			pass = false;
-		}
-		if (hw8.problem5(arr3, 9) == -1) {
-			System.out.println("Pass 5.2");
-		} else {
-			System.out.println("Fail 5.2");
-			pass = false;
-		}
-
-		if (pass) {
-			System.out.println("All Pass");
-		} else {
-			System.out.println("Fail");
-		}
+		return location;
 	}
+
+
+public static void main(String[] args) {
+
+      boolean pass = true;
+
+
+
+      Homework8 hw8 = new Homework8(11);
+
+
+
+      int[] arr = hw8.problem1();
+
+      if (arr.length == 11) {
+
+                      System.out.println("Pass 1");
+
+      } else {
+
+                      System.out.println("Fail 1");
+
+                      pass = false;
+
+      }
+
+
+
+      hw8.problem2(7);
+
+      boolean pass2 = true;
+
+      for (int n : arr) {
+
+                      if (n != 7) {
+
+                                      pass2 = false;
+
+                      }
+
+      }
+
+      if (pass2) {
+
+                      System.out.println("Pass 2");
+
+      } else {
+
+                      System.out.println("Fail 2");
+
+                      pass = false;
+
+      }
+
+
+
+      boolean pass3 = true;
+
+      int value = 13;
+
+      hw8.problem3(value);
+
+      for (int i = 0; i < arr.length; i++) {
+
+                      if (arr[i] != value) {
+
+                                      pass3 = false;
+
+                      }
+
+                      value--;
+
+      }
+
+      if (pass3) {
+
+                      System.out.println("Pass 3");
+
+      } else {
+
+                      System.out.println("Fail 3");
+
+                      pass = false;
+
+      }
+
+
+
+      double[] arr2 = {2.3, 3.5, 5.7, 7.11, 5.13, 3.17, 2.19};
+
+      if (hw8.problem4(arr2) == 7.11) {
+
+                      System.out.println("Pass 4");
+
+      } else {
+
+                      System.out.println("Fail 4");
+
+                      pass = false;
+
+      }
+
+
+
+      int[] arr3 = {2, 3, 5, 7, 11, 13};
+
+      if (hw8.problem5(arr3, 5) == 2) {
+
+                      System.out.println("Pass 5.1");
+
+      } else {
+
+                      System.out.println("Fail 5.1");
+
+                      pass = false;
+
+      }
+
+      if (hw8.problem5(arr3, 9) == -1) {
+
+                      System.out.println("Pass 5.2");
+
+      } else {
+
+                      System.out.println("Fail 5.2");
+
+                      pass = false;
+
+      }
+
+
+
+      if (pass) {
+
+                      System.out.println("All Pass");
+
+      } else {
+
+                      System.out.println("Fail");
+
+      }
+
+  }
 }
